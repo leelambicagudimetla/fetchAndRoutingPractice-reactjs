383f1a6ea6ea3244cbfbd143bda8cb655f123a3c
leelambicagudimetla <leelambicagudimetla@gmail.com>
2024-09-16 14:49:55 +0000

    fetch routing

---
+++ src/components/Home/index.js
@@ -0,0 +1,11 @@
+import './index.css'
+import UserInfo from '../UserInfo'
+
+const Home = () => (
+  <div>
+    <UserInfo />
+    <div className="home-container">Render UserInfo and BlogList</div>
+  </div>
+)
+
+export default Home
